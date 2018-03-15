d9b46aa9dc5e24cc4a7a55d62cc7c1ae4a360884
Alex Kovalevych <alexkovalevych@gmail.com>
2018-03-15 17:05:14 +0200

    [patch] fix tsvector index to use english dictionary

---
+++ priv/repo/migrations/20180315150322_fix_parties_full_text_index.exs
@@ -0,0 +1,14 @@
+defmodule Report.Repo.Migrations.FixPartiesFullTextIndex do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    execute("DROP INDEX full_name_idx")
+
+    execute("""
+    CREATE INDEX full_name_idx ON parties
+    USING GIN (to_tsvector('english', first_name || ' ' || last_name || ' ' || second_name));
+    """)
+  end
+end
